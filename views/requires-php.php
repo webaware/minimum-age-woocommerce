46c04ead6db223c29036d759e6e58276c4535ddc
Ross McKay <rmckay@webaware.com.au>
2018-11-03 20:11:23 +1100

    remove short echo tags from PHP 5.2 compatible view

--- views/requires-php.php
+++ views/requires-php.php
@@ -1,4 +1,6 @@
 <?php
+// NB: Minimum PHP version for this file is 5.3! No short array notation, no namespaces!
+
 if (!defined('ABSPATH')) {
 	exit;
 }
@@ -6,7 +8,7 @@ if (!defined('ABSPATH')) {
 
 <div class="notice notice-error">
 	<p>
-		<?= min_age_woo_external_link(
+		<?php echo min_age_woo_external_link(
 				sprintf(esc_html__('Minimum Age for WooCommerce requires PHP %1$s or higher; your website has PHP %2$s which is {{a}}old, obsolete, and unsupported{{/a}}.', 'minimum-age-woocommerce'),
 					esc_html(EWAY_PAYMENTS_MIN_PHP), esc_html(PHP_VERSION)),
 				'https://secure.php.net/supported-versions.php'
