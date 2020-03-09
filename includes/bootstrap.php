a7358c4ca03c41bc484bd8661d347bc9b134fb4a
Ross McKay <rmckay@webaware.com.au>
2020-03-09 21:34:22 +1100

    move constants to bootstrap.php

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -5,6 +5,16 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
+// prerequisites
+const MIN_VERSION_WOOCOMMERCE		= '3.0';
+
+// settings names
+const SETTING_CHECKOUT_TITLE		= 'min_age_woo_checkout_title';
+const SETTING_CHECKOUT_DESCRIPTION	= 'min_age_woo_checkout_description';
+const SETTING_ERROR_UNDER_AGE		= 'min_age_woo_error_under_age';
+const SETTING_MIN_AGE				= 'min_age_woo_min_age';
+const SETTING_DATE_LAYOUT			= 'min_age_woo_date_layout';
+
 /**
 * kick start the plugin
 */
